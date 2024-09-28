a210c67c4ce3c02abc2669e5d76879948021961b
Kattniu <kathe.adriana24@gmail.com>
2024-09-28 16:35:50 +0200

    notas

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,27 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Crear los trabajos
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Marketing";
+        job2._company = "Apple";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        // Crear el currículum
+        Resume myResume = new Resume();
+        myResume._name = "Katherine Gonzales";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Mostrar el currículum completo
+        myResume.Display();
     }
 }
